d09df2b6af6314e977421fbf4ff4a6225f26b4ea
VanessaLin9 <vanessa7591@gmail.com>
2024-11-17 16:21:25 +0800

    update q

--- WebApiC/Controllers/TodoController.cs
+++ WebApiC/Controllers/TodoController.cs
@@ -19,18 +19,20 @@ public class TodoController(TodoService todoService) : ControllerBase
     [HttpGet("/{id:int}")]
     public Todo? GetTodoById(int id)
     {
-        return todoService.GetTodoById(id);
+        var todoById = todoService.GetTodoById(id);
+
+
+        return todoById;
     }
 
+// 6. 使用 Controller base 來實作一個 POST API /todos 透過 body 可以建立一個 todo
 
-// 1. 使用 Controller base 來實作一個 POST API /todos 透過 body 可以建立一個 todo
+// 7. 使用 Controller base 來實作一個 PUT API /todos/{id} 加上 body 可以修改特定一個 todo 的內容
 
-// 2. 使用 Controller base 來實作一個 PUT API /todos/{id} 加上 body 可以修改特定一個 todo 的內容
+// 8. 使用 Controller base 來實作一個 DELETE API /todos/{id} 可以刪除特定一個 todo
 
-// 3. 使用 Controller base 來實作一個 DELETE API /todos/{id} 可以刪除特定一個 todo
+// 9. 使用 Controller base 來實作一個 POST API /todos?title={title}&dueby={dueby}&iscomplete={iscomplete} 不透過 body, 反而透過 query 來建立一個 todo 內容
 
-// 4. 使用 Controller base 來實作一個 POST API /todos?title={title}&dueby={dueby}&iscomplete={iscomplete} 不透過 body, 反而透過 query 來建立一個 todo 內容
-
-// 5. 使用 Controller base 來實作一個 GET API /todos/search?title={title} 可以取得包含查詢條件的的 title
+// 10. 使用 Controller base 來實作一個 GET API /todos/search?title={title} 可以取得包含查詢條件的的 titles
 
 }
