e37d45e0ae04a9126589e979399044fe826284ff
VanessaLin9 <vanessa7591@gmail.com>
2024-11-17 18:14:03 +0800

    fix type

--- WebApiC/Models/Todo.cs
+++ WebApiC/Models/Todo.cs
@@ -5,7 +5,7 @@ namespace WebApiC.Models;
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
-[JsonSerializable(typeof(Todo[]))]
+[JsonSerializable(typeof(List<Todo>))]
 
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
