b9b928418c442c2c055c5af53695053fe6d46ec2
VanessaLin9 <vanessa7591@gmail.com>
2024-11-20 04:48:24 +0800

    ✅ use action result

--- WebApiC/Controllers/TodoController.cs
+++ WebApiC/Controllers/TodoController.cs
@@ -26,13 +26,46 @@ public class TodoController(TodoService todoService) : ControllerBase
     }
 
 // 6. 使用 Controller base 來實作一個 POST API /todos 透過 body 可以建立一個 todo
+    [HttpPost("/")]
+    public OkObjectResult AddTodo(Todo todo)
+    {
+        var todoList = todoService.AddTodo(todo);
+        return Ok(todoList);
+    }
+
 
 // 7. 使用 Controller base 來實作一個 PUT API /todos/{id} 加上 body 可以修改特定一個 todo 的內容
+    [HttpPut("/id:int")]
+    public OkObjectResult PutTodo(int id, Todo todo)
+    {
+        var todos = todoService.PutTodo(id, todo);
+        return new OkObjectResult(todos!.Count == 0?NotFound() : Ok(todos));
+    }
+
 
 // 8. 使用 Controller base 來實作一個 DELETE API /todos/{id} 可以刪除特定一個 todo
+    [HttpDelete("/{id:int}")]
+    public OkObjectResult DeleteTodo(int id)
+    {
+        var todos = todoService.DeleteTodo(id);
+        return Ok(todos);
+    }
 
 // 9. 使用 Controller base 來實作一個 POST API /todos?title={title}&dueby={dueby}&iscomplete={iscomplete} 不透過 body, 反而透過 query 來建立一個 todo 內容
+    [HttpPost("/")]
+    public OkObjectResult AddTodoByQuery(string title, string? dueby , bool iscomplete)
+    {
+        var dateOnly = dueby is null ? DateOnly.ParseExact(dueby, "yyyy-MM-dd"): DateOnly.FromDateTime(DateTime.Now);
+        var todo = new Todo(0, title, dateOnly, iscomplete);
+        var addTodo = todoService.AddTodo(todo);
+        return Ok(addTodo);
+    } 
 
 // 10. 使用 Controller base 來實作一個 GET API /todos/search?title={title} 可以取得包含查詢條件的的 titles
-
+    [HttpGet("/search")]
+    public OkObjectResult FilterTodo(string title)
+    {
+        var filterTodo = todoService.FilterTodo(title);
+        return new OkObjectResult(filterTodo == null ? NotFound() : Ok(filterTodo));
+    }
 }
--- WebApiC/Services/TodoService.cs
+++ WebApiC/Services/TodoService.cs
@@ -4,7 +4,7 @@ namespace WebApiC.Services;
 
 public class TodoService
 {
-    private List<Todo?> _sampleList =
+    private List<Todo> _sampleList =
     [
         new(1, "Talk With Ann", DateOnly.FromDateTime(DateTime.Now), true),
         new(2, "Buy Some Drink"),
@@ -12,7 +12,7 @@ public class TodoService
     ];
 
 
-    public List<Todo?> GetAllTodo()
+    public List<Todo>? GetAllTodo()
     {
         return _sampleList;
     }
@@ -20,5 +20,38 @@ public class TodoService
     public Todo? GetTodoById(int id)
     {
         return _sampleList.FirstOrDefault(x => x!.Id == id);
+    }
+
+    public List<Todo>? AddTodo(Todo todo)
+    {
+        var id = _sampleList.Max(x => x!.Id) + 1;
+        _sampleList.Add(todo with{Id = id});
+        return _sampleList;
+    }
+
+    public List<Todo>? PutTodo(int id, Todo todo)
+    {
+        var modifyTodo = _sampleList.FindIndex(x => x!.Id == id);
+        if (modifyTodo == -1)
+        {
+            return null;
+        }
+
+        _sampleList[modifyTodo] = todo;
+        return _sampleList;
+    }
+
+    public List<Todo> DeleteTodo(int id)
+    {
+        var enumerable = _sampleList.First(x => x.Id == id);
+
+        _sampleList.Remove(enumerable);
+        return _sampleList;
+    }
+
+    public List<Todo>? FilterTodo(string title)
+    {
+        var enumerable = _sampleList.Where(x => x.Title!.Contains(title, StringComparison.OrdinalIgnoreCase));
+        return enumerable.ToList();
     }
 }
